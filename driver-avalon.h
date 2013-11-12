ecedd7721454a64cb64b8dba8124f2e10d68e8d0
Con Kolivas <kernel@kolivas.org>
2013-11-12 17:28:45 +1100

    Set avalon ftdi latency to just less than the time it would take to fill the ftdi buffer at 115200 baud

--- driver-avalon.h
+++ driver-avalon.h
@@ -65,7 +65,8 @@
 
 #define AVALON_FTDI_READSIZE 510
 #define AVALON_READBUF_SIZE 8192
-#define AVALON_LATENCY 16 /* This is basically the ftdi default anyway */
+/* Set latency to just less than full 64 byte packet size at 115200 baud */
+#define AVALON_LATENCY 4
 
 struct avalon_task {
 	uint8_t reset		:1;
