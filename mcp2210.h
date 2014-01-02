94a157ff504eeea405c3d68fe3b47aca5fc793ca
Con Kolivas <kernel@kolivas.org>
2014-01-03 10:03:13 +1100

    Add basic SPI comms defines for mcp2210 and build rules for bitfury.

---
+++ mcp2210.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2014 Con Kolivas
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
+#ifndef MCP2210_H
+#define MCP2210_H
+
+#define MCP2210_BUFFER_LENGTH		64
+
+#define MCP2210_GPIO_OUTPUT		0
+#define MCP2210_GPIO_INPUT		1
+
+#define MCP2210_GET_GPIO_SETTING	0x20
+#define MCP2210_SET_GPIO_SETTING	0x21
+#define MCP2210_SET_GPIO_PIN_VAL	0x30
+#define MCP2210_GET_GPIO_PIN_VAL	0x31
+#define MCP2210_SET_GPIO_PIN_DIR	0x32
+#define MCP2210_GET_GPIO_PIN_DIR	0x33
+#define MCP2210_SET_SPI_SETTING		0X40
+#define MCP2210_GET_SPI_SETTING		0X41
+#define MCP2210_SPI_TRANSFER		0x42
+
+#define MCP2210_SPI_TRANSFER_SUCCESS	0x00
+#define MCP2210_SPI_TRANSFER_ERROR_NA	0xF7	// SPI not available due to external owner
+#define MCP2210_SPI_TRANSFER_ERROR_IP	0xF8	// SPI not available due to transfer in progress
+
+#endif MCP2210_H
