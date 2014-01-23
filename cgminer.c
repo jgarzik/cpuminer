92e1887f8f71c55f581b052993d93ab30a64a18c
Con Kolivas <kernel@kolivas.org>
2014-01-23 23:36:50 +1100

    Change new block detection message since longpoll is rarely relevant today.

--- cgminer.c
+++ cgminer.c
@@ -4082,7 +4082,7 @@ static bool test_work_current(struct work *work)
 				       work->gbt ? "GBT " : "", work->pool->pool_no);
 			}
 		} else if (have_longpoll)
-			applog(LOG_NOTICE, "New block detected on network before longpoll");
+			applog(LOG_NOTICE, "New block detected on network before pool notification");
 		else
 			applog(LOG_NOTICE, "New block detected on network");
 		restart_threads();
