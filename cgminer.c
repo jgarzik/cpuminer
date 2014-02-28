2b702ce28d305f83479a5b22955b9518923e3447
Con Kolivas <kernel@kolivas.org>
2014-02-28 12:43:53 +1100

    Refresh the log window on pool failure message at startup.

--- cgminer.c
+++ cgminer.c
@@ -8893,6 +8893,7 @@ int main(int argc, char *argv[])
 		if (!use_curses)
 			early_quit(0, "No servers could be used! Exiting.");
 #ifdef HAVE_CURSES
+		wrefresh(logwin);
 		halfdelay(10);
 		if (getch() != ERR)
 			early_quit(0, "No servers could be used! Exiting.");
