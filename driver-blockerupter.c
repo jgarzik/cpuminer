20d89f11cdca48e0d0cc81f61e43162537ca1f51
Con Kolivas <kernel@kolivas.org>
2014-10-14 11:22:03 +1100

    Fix bulk of remaining style in blockerupter.c

--- driver-blockerupter.c
+++ driver-blockerupter.c
@@ -53,6 +53,7 @@ static void blockerupter_mark_mode(struct cgpu_info *blockerupter)
 
 static void blockerupter_init_com(struct cgpu_info *blockerupter)
 {
+	uint32_t baudrate;
 	int interface;
 
 	if (blockerupter->usbinfo.nodev)
@@ -61,24 +62,26 @@ static void blockerupter_init_com(struct cgpu_info *blockerupter)
 	interface = usb_interface(blockerupter);
 
 	// Enable the UART
-	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_IFC_ENABLE, CP210X_VALUE_UART_ENABLE, interface, NULL, 0, C_ENABLE_UART);
+	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_IFC_ENABLE,
+			  CP210X_VALUE_UART_ENABLE, interface, NULL, 0, C_ENABLE_UART);
 	if (blockerupter->usbinfo.nodev)
 		return;
 
 	// Set data control
-	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_DATA, CP210X_VALUE_DATA, interface, NULL, 0, C_SETDATA);
+	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_DATA, CP210X_VALUE_DATA,
+			  interface, NULL, 0, C_SETDATA);
 
 	if (blockerupter->usbinfo.nodev)
 		return;
 
 	// Set the baud
-	uint32_t baudrate = BET_BAUD;
+	baudrate = BET_BAUD;
 
-	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_BAUD, 0, interface, &baudrate, sizeof (baudrate), C_SETBAUD);
+	usb_transfer_data(blockerupter, CP210X_TYPE_OUT, CP210X_REQUEST_BAUD, 0,
+			  interface, &baudrate, sizeof (baudrate), C_SETBAUD);
 
 	// Set space mode
 	blockerupter_space_mode(blockerupter);
-
 }
 
 static int blockerupter_send(struct cgpu_info *blockerupter, char *data, int len)
@@ -117,7 +120,7 @@ static int blockerupter_read(struct cgpu_info *blockerupter, char *data, int len
 	return READ_OK;
 }
 
-static void blockerupter_setclock(struct cgpu_info *blockerupter,uint8_t clock)
+static void blockerupter_setclock(struct cgpu_info *blockerupter, uint8_t clock)
 {
 	struct blockerupter_info *info;
 	info = blockerupter->device_data;
@@ -126,13 +129,13 @@ static void blockerupter_setclock(struct cgpu_info *blockerupter,uint8_t clock)
 
 	command = C_GCK | clock;
 	info->clock = clock;
-	err = blockerupter_send(blockerupter,&command,1);
-	if (!err) {
-		applog(LOG_DEBUG,"%s%d: Set Clock to %d MHz",blockerupter->drv->name, blockerupter->device_id,(clock+1)*10/2);
-	}
+	err = blockerupter_send(blockerupter, &command, 1);
+	if (!err)
+		applog(LOG_DEBUG, "%s%d: Set Clock to %d MHz", blockerupter->drv->name,
+		       blockerupter->device_id, (clock + 1) * 10 / 2);
 }
 
-static void blockerupter_setdiff(struct cgpu_info *blockerupter,int diff)
+static void blockerupter_setdiff(struct cgpu_info *blockerupter, int diff)
 {
 	struct blockerupter_info *info;
 	info = blockerupter->device_data;
@@ -152,18 +155,19 @@ static void blockerupter_setdiff(struct cgpu_info *blockerupter,int diff)
 		local_diff = 64;
 	}
 
-	if (local_diff==info->diff)
+	if (local_diff == info->diff)
 		return;
 
 	command = C_DIF | bits;
-	err = blockerupter_send(blockerupter,&command,1);
+	err = blockerupter_send(blockerupter, &command, 1);
 	if (!err) {
-		applog(LOG_DEBUG,"%s%d: Set Diff Bits to %d",blockerupter->drv->name, blockerupter->device_id, bits);
+		applog(LOG_DEBUG, "%s%d: Set Diff Bits to %d", blockerupter->drv->name,
+		       blockerupter->device_id, bits);
 		info->diff = local_diff;
 	}
 }
 
-static void blockerupter_setrolling(struct cgpu_info *blockerupter,uint8_t rolling)
+static void blockerupter_setrolling(struct cgpu_info *blockerupter, uint8_t rolling)
 {
 	struct blockerupter_info *info;
 	info = blockerupter->device_data;
@@ -171,11 +175,12 @@ static void blockerupter_setrolling(struct cgpu_info *blockerupter,uint8_t rolli
 	int err;
 
 	command = C_LPO | rolling;
-	err = blockerupter_send(blockerupter,&command,1);
+	err = blockerupter_send(blockerupter, &command, 1);
 
 	if (!err) {
-		applog(LOG_DEBUG,"%s%d: Set nTime Rolling to %d seconds",blockerupter->drv->name, blockerupter->device_id, (rolling+1)*30);
-		info->rolling = (rolling+1)*30;
+		applog(LOG_DEBUG, "%s%d: Set nTime Rolling to %d seconds", blockerupter->drv->name,
+		       blockerupter->device_id, (rolling + 1) * 30);
+		info->rolling = (rolling + 1) * 30;
 	}
 }
 
@@ -189,14 +194,14 @@ static void blockerupter_init(struct cgpu_info *blockerupter)
 		opt_bet_clk = BET_CLOCK_DEFAULT;
 	}
 	blockerupter_setclock(blockerupter, opt_bet_clk);
-	info->clock = (opt_bet_clk+1)*10;
-	info->expected = info->clock * 24 * 32 * info->found/1000.0;
+	info->clock = (opt_bet_clk + 1) * 10;
+	info->expected = info->clock * 24 * 32 * info->found / 1000.0;
 	// Set Diff
 	blockerupter_setdiff(blockerupter, BET_DIFF_DEFAULT);
 	info->diff = BET_DIFF_DEFAULT;
 	// Set nTime Rolling
 	blockerupter_setrolling(blockerupter, BET_ROLLING_DEFAULT);
-	info->rolling = (BET_ROLLING_DEFAULT+1)*30;
+	info->rolling = (BET_ROLLING_DEFAULT + 1) * 30;
 	cgtime(&info->start_time);
 }
 
@@ -213,9 +218,9 @@ static struct cgpu_info *blockerupter_detect_one(struct libusb_device *dev, stru
 		return NULL;
 	}
 
-	blockerupter->device_data = (struct blockerupter_info *) malloc(sizeof (struct blockerupter_info));
+	blockerupter->device_data = (struct blockerupter_info *) malloc(sizeof(struct blockerupter_info));
 	info = blockerupter->device_data;
-	memset(info, 0, sizeof (blockerupter_info));
+	memset(info, 0, sizeof(blockerupter_info));
 	blockerupter_init_com(blockerupter);
 
 	err = blockerupter_send(blockerupter, &reset, 1);
@@ -226,19 +231,21 @@ static struct cgpu_info *blockerupter_detect_one(struct libusb_device *dev, stru
 	}
 	cgsleep_ms(5000);
 
-	char detect, answer;
 
 	for (i = 0; i < BET_MAXBOARDS; i++) {
+		char detect, answer;
+
 		answer = 0;
 		detect = C_ASK | (uint8_t)i;
 		blockerupter_send(blockerupter, &detect, 1);
 		blockerupter_read(blockerupter, &answer, 1);
 		if (answer == A_WAL) {
-		     applog(LOG_DEBUG, "BlockEruper found Board: %d", i);
+		     applog(LOG_DEBUG, "BlockErupter found Board: %d", i);
 		     info->boards[i] = 1;
 		     info->found++;
 		} else {
-		     applog(LOG_DEBUG, "BlockErupter missing board: %d, received %02x", i, answer);
+		     applog(LOG_DEBUG, "BlockErupter missing board: %d, received %02x",
+			    i, answer);
 		}
 	}
 
@@ -249,7 +256,8 @@ static struct cgpu_info *blockerupter_detect_one(struct libusb_device *dev, stru
 	} else {
 		blockerupter->threads = 1;
 		add_cgpu(blockerupter);
-		applog(LOG_DEBUG, "Add BlockErupter with %d/%d Boards", info->found, BET_MAXBOARDS);
+		applog(LOG_DEBUG, "Add BlockErupter with %d/%d Boards", info->found,
+		       BET_MAXBOARDS);
 		blockerupter_init(blockerupter);
 		return blockerupter;
 	}
@@ -264,15 +272,15 @@ static struct api_data *blockerupter_api_stats(struct cgpu_info *blockerupter)
 {
 	struct blockerupter_info *info = blockerupter->device_data;
 	struct api_data *root = NULL;
-	struct timeval now,elapsed;
+	struct timeval now, elapsed;
 	char buf[32];
 	int i;
 
 	cgtime(&now);
-	timersub(&now,&info->start_time,&elapsed);
+	timersub(&now, &info->start_time, &elapsed);
 
-	info->hashrate = elapsed.tv_sec?info->hashes*4.295/elapsed.tv_sec:0;
-	info->eff = info->hashrate/info->expected;
+	info->hashrate = elapsed.tv_sec ? info->hashes * 4.295 / elapsed.tv_sec : 0;
+	info->eff = info->hashrate / info->expected;
 
 	root = api_add_int(root, "Nonces", &info->nonces, false);
 	root = api_add_uint8(root, "Board", &info->found, false);
@@ -281,17 +289,18 @@ static struct api_data *blockerupter_api_stats(struct cgpu_info *blockerupter)
 	root = api_add_double(root, "HashRate", &info->hashrate , false);
 	root = api_add_double(root, "Expected", &info->expected , false);
 	root = api_add_double(root, "Efficiency", &info->eff, false);
-	for (i=0;i<BET_MAXBOARDS;i++) {
+	for (i = 0; i < BET_MAXBOARDS; i++) {
 		double brd_hashrate;
+
 		if (info->boards[i]) {
-		     sprintf(buf,"Board%02d accepted",i);
+		     sprintf(buf, "Board%02d accepted", i);
 		     root = api_add_int(root, buf, &info->b_info[i].accepted, false);
-		     sprintf(buf,"Board%02d nonces",i);
+		     sprintf(buf, "Board%02d nonces", i);
 		     root = api_add_int(root, buf, &info->b_info[i].nonces, false);
-		     sprintf(buf,"Board%02d hwerror",i);
+		     sprintf(buf, "Board%02d hwerror", i);
 		     root = api_add_double(root, buf, &info->b_info[i].hwe, false);
-		     sprintf(buf,"Board%02d hashrate",i);
-		     brd_hashrate = elapsed.tv_sec?info->b_info[i].hashes*4.295/elapsed.tv_sec:0;
+		     sprintf(buf, "Board%02d hashrate", i);
+		     brd_hashrate = elapsed.tv_sec ? info->b_info[i].hashes * 4.295 / elapsed.tv_sec : 0;
 		     root = api_add_double(root, buf, &brd_hashrate, false);
 		}
 	}
@@ -329,7 +338,7 @@ static void blockerupter_sendjob(struct cgpu_info *blockerupter, int board)
 
 	blockerupter_send(blockerupter, (char *)(work->midstate), 32);
 	blockerupter_send(blockerupter, (char *)&(work->data[64]), 12);
-	blockerupter_send(blockerupter, (char *) &work->nonce2, 4);
+	blockerupter_send(blockerupter, (char *)&work->nonce2, 4);
 	blockerupter_send(blockerupter, (char *)&info->work_idx, 1);
 
 	cgsleep_ms(1);
@@ -344,14 +353,13 @@ static void blockerupter_sendjob(struct cgpu_info *blockerupter, int board)
 		applog(LOG_ERR, "%s%d: Sync Error", blockerupter->drv->name, blockerupter->device_id);
 	} else {
 		info->b_info[board].job_count++;
-		applog(LOG_DEBUG, "%s%d: Sent work %d to board %d", blockerupter->drv->name, blockerupter->device_id, info->work_idx, board);
+		applog(LOG_DEBUG, "%s%d: Sent work %d to board %d", blockerupter->drv->name,
+		       blockerupter->device_id, info->work_idx, board);
 	}
-
 
 	info->work_idx++;
-	if (info->work_idx>=BET_WORK_FIFO) {
+	if (info->work_idx >= BET_WORK_FIFO)
 		info->work_idx = 0;
-	}
 }
 
 static uint64_t blockerupter_checknonce(struct cgpu_info *blockerupter, struct blockerupter_response *resp, int board)
@@ -371,10 +379,12 @@ static uint64_t blockerupter_checknonce(struct cgpu_info *blockerupter, struct b
 
 	nonce = *(uint32_t *)resp->nonce;
 
-	applog(LOG_DEBUG, "%s%d: Nonce %08x from board %d, asic %d for work %d", blockerupter->drv->name, blockerupter->device_id, *(uint32_t *) resp->nonce, board, resp->chip, resp->work_idx);
+	applog(LOG_DEBUG, "%s%d: Nonce %08x from board %d, asic %d for work %d",
+	       blockerupter->drv->name, blockerupter->device_id, *(uint32_t *) resp->nonce,
+	       board, resp->chip, resp->work_idx);
 
-	memcpy(work.data+4+32+32,resp->ntime,4);
-	__bin2hex(work.ntime,resp->ntime,4);
+	memcpy(work.data + 4 + 32 + 32, resp->ntime, 4);
+	__bin2hex(work.ntime, resp->ntime, 4);
 
 	info->nonces++;
 	cur_brd = &info->b_info[board];
@@ -382,33 +392,33 @@ static uint64_t blockerupter_checknonce(struct cgpu_info *blockerupter, struct b
 	cur_asic = &info->b_info[board].asics[resp->chip];
 	cur_asic->nonces++;
 
-
 	for (i = 0; i < BET_NONCE_FIX; i++) {
 		test = test_nonce_diff(&work, nonce + i, (double)info->diff);
 		if (test) {
-		     applog(LOG_DEBUG, "%s%d: Nonce Fix Pass @%d", blockerupter->drv->name, blockerupter->device_id, i);
+		     applog(LOG_DEBUG, "%s%d: Nonce Fix Pass @%d", blockerupter->drv->name,
+			    blockerupter->device_id, i);
 		     info->hashes += info->diff;
 		     cur_brd->hashes += info->diff;
 		     cur_asic->hashes += info->diff;
 		     if (test_nonce_diff(&work, nonce + i, work.work_difficulty)) {
-		    if (submit_nonce(thr, &work, nonce + i)) {
-			 hashes += floor(work.work_difficulty) * (uint64_t) 0xffffffff;
-			 info->accepted++;
-			 cur_brd->accepted++;
-			 cur_asic->accepted++;
-		    }
+			if (submit_nonce(thr, &work, nonce + i)) {
+				hashes += floor(work.work_difficulty) * (uint64_t) 0xffffffff;
+				info->accepted++;
+				cur_brd->accepted++;
+				cur_asic->accepted++;
+			}
 		     }
 		     break;
 		}
 	}
 
-	if (i==BET_NONCE_FIX) {
-		applog(LOG_DEBUG, "%s%d: Nonce Fix Failed", blockerupter->drv->name, blockerupter->device_id);
+	if (i == BET_NONCE_FIX) {
+		applog(LOG_DEBUG, "%s%d: Nonce Fix Failed", blockerupter->drv->name,
+		       blockerupter->device_id);
 		cur_brd->bad++;
-		cur_brd->hwe = cur_brd->nonces?(double)cur_brd->bad/cur_brd->nonces:0;
+		cur_brd->hwe = cur_brd->nonces ? (double)cur_brd->bad / cur_brd->nonces : 0;
 		cur_asic->bad++;
-		cur_asic->hwe = cur_asic->nonces?(double)cur_asic->bad/cur_asic->nonces:0;
-
+		cur_asic->hwe = cur_asic->nonces ? (double)cur_asic->bad / cur_asic->nonces : 0;
 	}
 	return hashes;
 }
@@ -421,9 +431,8 @@ static uint64_t blockerupter_getresp(struct cgpu_info *blockerupter, int board)
 
 	resp = (struct blockerupter_response *) malloc(BET_RESP_SZ);
 	err = blockerupter_read(blockerupter, (char *)resp, BET_RESP_SZ);
-	if (!err) {
+	if (!err)
 		hashes = blockerupter_checknonce(blockerupter, resp, board);
-	}
 	free(resp);
 	return hashes;
 }
@@ -438,12 +447,14 @@ static int64_t blockerupter_scanhash(struct thr_info *thr)
 	int64_t hashes=0;
 
 	if (unlikely(blockerupter->usbinfo.nodev)) {
-		applog(LOG_ERR, "%s%d: Device disappeared, shutting down thread", blockerupter->drv->name, blockerupter->device_id);
+		applog(LOG_ERR, "%s%d: Device disappeared, shutting down thread",
+		       blockerupter->drv->name, blockerupter->device_id);
 		return -1;
 	}
 
 	for (i = 0; i < BET_MAXBOARDS; i++) {
-		if (!info->boards[i]) continue;
+		if (!info->boards[i])
+			continue;
 		ask = C_ASK | (uint8_t)i;
 		blockerupter_send(blockerupter, &ask, 1);
 		cgsleep_ms(1);
@@ -460,7 +471,8 @@ static int64_t blockerupter_scanhash(struct thr_info *thr)
 		case A_NO:
 		     break;
 		default:
-		     applog(LOG_ERR, "%s%d: Unexpected value %02x received", blockerupter->drv->name, blockerupter->device_id, answer);
+		     applog(LOG_ERR, "%s%d: Unexpected value %02x received", blockerupter->drv->name,
+			    blockerupter->device_id, answer);
 		     break;
 		}
 	}
