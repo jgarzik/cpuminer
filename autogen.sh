2c98f1b8579f30e6af8c887dc5a843a1e8d63842
James Hilliard <james.hilliard1@gmail.com>
2014-11-21 14:13:55 -0700

    "readlink -f" is not portable to osx

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bs_dir="$(dirname $(readlink -f $0))"
+bs_dir="$(dirname "$0")"
 
 #Some versions of libtoolize don't like there being no ltmain.sh file already
 touch "${bs_dir}"/ltmain.sh
