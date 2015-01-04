e9f68f2bc5ee11cc63f0d072a9139b330ec6424f
Mikeqin <Fengling.Qin@gmail.com>
2015-01-04 16:58:31 +0800

    Support auto adjust voltage (MM-4.1)

--- driver-avalon4.c
+++ driver-avalon4.c
@@ -1423,11 +1423,19 @@ static int64_t avalon4_scanhash(struct thr_info *thr)
 			hwp = a ? (double)b / (double)a : 0;
 			if (hwp > AVA4_DH_INC && (info->set_voltage[i] < info->set_voltage[0] + 125)) {
 				info->set_voltage[i] += 125;
+				for (j = 0; j < AVA4_DEFAULT_MINERS; j++) {
+					info->set_voltage_i[i][j] += 125;
+				}
+
 				applog(LOG_NOTICE, "%s %d: Automatic increase module[%d] voltage to %d",
 				       avalon4->drv->name, avalon4->device_id, i, info->set_voltage[i]);
 			}
 			if (hwp < AVA4_DH_DEC && (info->set_voltage[i] > info->set_voltage[0] - (4 * 125))) {
 				info->set_voltage[i] -= 125;
+				for (j = 0; j < AVA4_DEFAULT_MINERS; j++) {
+					info->set_voltage_i[i][j] -= 125;
+				}
+
 				applog(LOG_NOTICE, "%s %d: Automatic decrease module[%d] voltage to %d",
 				       avalon4->drv->name, avalon4->device_id, i, info->set_voltage[i]);
 			}
