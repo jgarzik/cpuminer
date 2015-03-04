57de2ccda7490d626b7484f930893bafde3581d8
Con Kolivas <kernel@kolivas.org>
2015-03-05 10:15:37 +1100

    Check for attempts to copy to/from null in cg_memcpy

--- util.c
+++ util.c
@@ -3276,7 +3276,17 @@ void _cg_memcpy(void *dest, const void *src, unsigned int n, const char *file, c
 {
 	if (unlikely(n < 1 || n > (1ul << 31))) {
 		applog(LOG_ERR, "ERR: Asked to memcpy %u bytes from %s %s():%d",
-			      n, file, func, line);
+		       n, file, func, line);
+		return;
+	}
+	if (unlikely(!dest)) {
+		applog(LOG_ERR, "ERR: Asked to memcpy %u bytes to NULL from %s %s():%d",
+		       n, file, func, line);
+		return;
+	}
+	if (unlikely(!src)) {
+		applog(LOG_ERR, "ERR: Asked to memcpy %u bytes from NULL from %s %s():%d",
+		       n, file, func, line);
 		return;
 	}
 	memcpy(dest, src, n);
