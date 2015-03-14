95648050131c722a2981fa69b07edc2ec9e3db9c
Con Kolivas <kernel@kolivas.org>
2015-03-14 14:35:01 +1100

    Remove LT_INIT from configure which breaks ltmain.sh detection for some automagic reason

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 bs_dir=$(cd "$(dirname "$0")"; pwd)
 
-#Some versions of libtoolize don't like there being no ltmain.sh file already
-touch "${bs_dir}"/ltmain.sh
 autoreconf -fi "${bs_dir}"
 
 if test -n "$1" && test -z "$NOCONFIGURE" ; then
