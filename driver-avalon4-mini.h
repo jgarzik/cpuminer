8bfe63c2ff0a5dfd4e792ff3b62b383902ab8cf4
Mikeqin <Fengling.Qin@gmail.com>
2015-05-06 14:06:14 +0800

    Update AVAM_DEFAULT_ARRAY_SIZE

--- driver-avalon4-mini.h
+++ driver-avalon4-mini.h
@@ -15,7 +15,7 @@
 #ifdef USE_AVALON4_MINI
 
 #define AVAM_DEFAULT_ASIC_COUNT		4
-#define AVAM_DEFAULT_ARRAY_SIZE		(3 + 2) /* This is from the A3222 datasheet */
+#define AVAM_DEFAULT_ARRAY_SIZE		(3 + 1) /* This is from the A3222 datasheet. 3 quequed work + 1 new work */
 
 #define AVAM_DEFAULT_FREQUENCY_MIN	100
 #define AVAM_DEFAULT_FREQUENCY_MAX	1000
