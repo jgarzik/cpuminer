1cd6555263dc7a915923e69c677d859dce89cdce
Mikeqin <Fengling.Qin@gmail.com>
2015-09-30 11:37:01 +0800

    Show ASCI status for Avalon6

--- driver-avalon4.c
+++ driver-avalon4.c
@@ -2112,7 +2112,7 @@ static struct api_data *avalon4_api_stats(struct cgpu_info *cgpu)
 		if (info->mod_type[i] == AVA4_TYPE_NULL)
 			continue;
 
-		if (info->mod_type[i] == AVA4_TYPE_MM50) {
+		if (info->mod_type[i] == AVA4_TYPE_MM50 || info->mod_type[i] == AVA4_TYPE_MM60) {
 			for (j = 0; j < info->miner_count[i]; j++) {
 				sprintf(buf, " MW%d[", j);
 				strcat(statbuf[i], buf);
