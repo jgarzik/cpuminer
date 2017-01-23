e8becaae414e7e13f12bd25833cd96ea86f73cc8
Con Kolivas <kernel@kolivas.org>
2017-01-23 18:21:36 +1100

    Increase header size.

--- util.c
+++ util.c
@@ -1969,7 +1969,7 @@ static char *blank_merkle = "000000000000000000000000000000000000000000000000000
 static bool parse_notify(struct pool *pool, json_t *val)
 {
 	char *job_id, *prev_hash, *coinbase1, *coinbase2, *bbversion, *nbit,
-	     *ntime, header[228];
+	     *ntime, header[260];
 	unsigned char *cb1 = NULL, *cb2 = NULL;
 	size_t cb1_len, cb2_len, alloc_len;
 	bool clean, ret = false;
@@ -2052,7 +2052,7 @@ static bool parse_notify(struct pool *pool, json_t *val)
 	/* nonce */		 8 +
 	/* workpadding */	 96;
 #endif
-	snprintf(header, 225,
+	snprintf(header, 257,
 		"%s%s%s%s%s%s%s",
 		pool->bbversion,
 		pool->prev_hash,
@@ -2061,7 +2061,8 @@ static bool parse_notify(struct pool *pool, json_t *val)
 		pool->nbit,
 		"00000000", /* nonce */
 		workpadding);
-	ret = hex2bin(pool->header_bin, header, 112);
+
+	ret = hex2bin(pool->header_bin, header, 128);
 	if (unlikely(!ret)) {
 		applog(LOG_ERR, "Failed to convert header to header_bin in parse_notify");
 		goto out_unlock;
