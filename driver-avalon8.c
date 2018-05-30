0ffd9242d8ee20537efe3d9b08ad7d7d527f0f2e
xzx <xuzhenxing@canaan-creative.com>
2018-05-30 14:17:12 +0800

    Update api debug message for A851

--- driver-avalon8.c
+++ driver-avalon8.c
@@ -2246,29 +2246,97 @@ static struct api_data *avalon8_api_stats(struct cgpu_info *avalon8)
 			}
 			statbuf[strlen(statbuf) - 1] = ']';
 
-			for (j = 0; j < info->miner_count[i]; j++) {
-				sprintf(buf, " PVT_T%d[", j);
-				strcat(statbuf, buf);
-				for (k = 0; k < info->asic_count[i]; k++) {
-					sprintf(buf, "%d ", info->temp[i][j][k]);
+			if (!strncmp((char *)&(info->mm_version[i]), "851", 3)) {
+				for (j = 0; j < info->miner_count[i]; j++) {
+					sprintf(buf, " PVT_T%d[", j);
 					strcat(statbuf, buf);
-				}
-
-				statbuf[strlen(statbuf) - 1] = ']';
-				statbuf[strlen(statbuf)] = '\0';
-			}
-
-			for (j = 0; j < info->miner_count[i]; j++) {
-				for (k = 0; k < info->asic_count[i]; k++) {
-					sprintf(buf, " PVT_V%d_%d[", j, k);
-					strcat(statbuf, buf);
-					for (m = 0; m < AVA8_DEFAULT_CORE_VOLT_CNT; m++) {
-						sprintf(buf, "%d ", info->core_volt[i][j][k][m]);
+					for (k = 0; k < info->asic_count[i]; k++) {
+						sprintf(buf, "%3d ", info->temp[i][j][k]);
 						strcat(statbuf, buf);
 					}
 
 					statbuf[strlen(statbuf) - 1] = ']';
 					statbuf[strlen(statbuf)] = '\0';
+				}
+
+				for (j = 0; j < info->miner_count[i]; j++) {
+					sprintf(buf, " PVT_V%d[", j);
+					strcat(statbuf, buf);
+					for (k = 0; k < info->asic_count[i]; k++) {
+						sprintf(buf, "%d ", info->core_volt[i][j][k][0]);
+						strcat(statbuf, buf);
+					}
+
+					statbuf[strlen(statbuf) - 1] = ']';
+					statbuf[strlen(statbuf)] = '\0';
+				}
+
+				for (j = 0; j < info->miner_count[i]; j++) {
+					sprintf(buf, " ERATIO%d[", j);
+					strcat(statbuf, buf);
+					for (k = 0; k < info->asic_count[i]; k++) {
+						if (info->get_asic[i][j][k][0])
+							sprintf(buf, "%6.2f%% ", (double)(info->get_asic[i][j][k][1] * 100.0 / (info->get_asic[i][j][k][0] + info->get_asic[i][j][k][1])));
+						else
+							sprintf(buf, "%6.2f%% ", 0.0);
+						strcat(statbuf, buf);
+					}
+
+					statbuf[strlen(statbuf) - 1] = ']';
+				}
+
+				int l;
+				/* i: modular, j: miner, k:asic, l:value */
+				for (l = 0; l < 2; l++) {
+					for (j = 0; j < info->miner_count[i]; j++) {
+						sprintf(buf, " C_%02d_%02d[", j, l);
+						strcat(statbuf, buf);
+						for (k = 0; k < info->asic_count[i]; k++) {
+							sprintf(buf, "%7d ", info->get_asic[i][j][k][l]);
+							strcat(statbuf, buf);
+						}
+
+						statbuf[strlen(statbuf) - 1] = ']';
+					}
+				}
+
+				for (j = 0; j < info->miner_count[i]; j++) {
+					sprintf(buf, " GHSmm%02d[", j);
+					strcat(statbuf, buf);
+					for (k = 0; k < info->asic_count[i]; k++) {
+						mhsmm = 0;
+						for (l = 2; l < 6; l++)
+							mhsmm += (info->get_asic[i][j][k][l] * info->set_frequency[i][j][l - 2]);
+						sprintf(buf, "%7.2f ", mhsmm / 1000);
+						strcat(statbuf, buf);
+					}
+					statbuf[strlen(statbuf) - 1] = ']';
+				}
+			} else {
+				for (j = 0; j < info->miner_count[i]; j++) {
+					sprintf(buf, " PVT_T%d[", j);
+					strcat(statbuf, buf);
+					for (k = 0; k < info->asic_count[i]; k++) {
+						sprintf(buf, "%d ", info->temp[i][j][k]);
+						strcat(statbuf, buf);
+					}
+
+					statbuf[strlen(statbuf) - 1] = ']';
+					statbuf[strlen(statbuf)] = '\0';
+				}
+
+				for (j = 0; j < info->miner_count[i]; j++) {
+					for (k = 0; k < info->asic_count[i]; k++) {
+						sprintf(buf, " PVT_V%d_%d[", j, k);
+						strcat(statbuf, buf);
+						for (m = 0; m < AVA8_DEFAULT_CORE_VOLT_CNT; m++) {
+							sprintf(buf, "%d ", info->core_volt[i][j][k][m]);
+							strcat(statbuf, buf);
+						}
+
+						statbuf[strlen(statbuf) - 1] = ']';
+						statbuf[strlen(statbuf)] = '\0';
+					}
 				}
 			}
 		}
