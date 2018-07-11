2abc26576583bc731979f50458d867807b07ff89
Con Kolivas <kernel@kolivas.org>
2018-07-11 12:28:47 +1000

    Cope with abs timeouts not working on some OSs, fixing a logic error on cgsem_mswait.

--- util.c
+++ util.c
@@ -3572,17 +3572,24 @@ retry:
 int _cgsem_mswait(cgsem_t *cgsem, int ms, const char *file, const char *func, const int line)
 {
 	struct timespec abs_timeout, ts_now;
-	struct timeval tv_now;
+	struct timeval tv_now, abs;
 	int ret;
 
 	cgtime(&tv_now);
 	timeval_to_spec(&ts_now, &tv_now);
 	ms_to_timespec(&abs_timeout, ms);
-retry:
 	timeraddspec(&abs_timeout, &ts_now);
+	timespec_to_val(&abs, &abs_timeout);
+retry:
 	ret = sem_timedwait(cgsem, &abs_timeout);
 
 	if (ret) {
+		cgtime(&tv_now);
+		/* Some OSs have unreliable abs timeouts, so fake it out! */
+		if (time_less(&tv_now, &abs)) {
+			cgsleep_ms(10);
+			goto retry;
+		}
 		if (likely(sock_timeout()))
 			return ETIMEDOUT;
 		if (interrupted())
