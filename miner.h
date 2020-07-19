d464b20a34d75e6a85e268a9eedfad0c78a1faa0
Jeff Garzik <jeff@bloq.com>
2020-07-19 10:40:33 -0400

    Minor clarity of license.

--- miner.h
+++ miner.h
@@ -1,6 +1,13 @@
 #ifndef __MINER_H__
 #define __MINER_H__
 
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.  See COPYING for more details.
+ */
+
 #include "cpuminer-config.h"
 
 #include <stdbool.h>
